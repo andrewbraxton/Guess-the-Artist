152e545f59165e52b68a40338dae36d739c11e65
Nigel Tracey <ntracey2@illinois.edu>
2018-12-02 17:56:14 -0600

    added randomized artist buttons

--- app/src/main/java/edu/illinois/cs/cs125/cs125mp6/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/cs125mp6/MainActivity.java
@@ -18,6 +18,9 @@ import com.google.gson.JsonObject;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     /**
@@ -30,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private static RequestQueue requestQueue;
 
+    private static ArrayList<String> songs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,22 +42,71 @@ public class MainActivity extends AppCompatActivity {
 
         requestQueue = Volley.newRequestQueue(this);
 
+        songs = new ArrayList<String>();
+        songs.add("Young Thug-Halftime");
+        songs.add("Denzel Curry-Ultimate");
+        songs.add("Travis Scott-Sicko Mode");
+        songs.add("Chief Keef-Faneto");
+        songs.add("Rush-Tom Sawyer");
+
+        String answer = "";
+
         // Attach the handler to our UI button
+
+
+        final Button choice1 = findViewById(R.id.choice1);
+        choice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Choice 1 button clicked");
+            }
+        });
+        final Button choice2 = findViewById(R.id.choice2);
+        choice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Choice 2 button clicked");
+            }
+        });
+        final Button choice3 = findViewById(R.id.choice3);
+        choice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Choice 3 button clicked");
+            }
+        });
+
         final Button newLyrics = findViewById(R.id.newLyrics);
         newLyrics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "New Lyrics button clicked");
-                getNewLyrics();
+
+                String s1 = songs.get(new Random().nextInt(songs.size()));
+                choice1.setText(s1.split("-")[0]);
+                choice1.setTag(s1.split("-")[1]);
+                String s2 = songs.get(new Random().nextInt(songs.size()));
+                choice2.setText(s2.split("-")[0]);
+                choice2.setTag(s2.split("-")[1]);
+                String s3 = songs.get(new Random().nextInt(songs.size()));
+                choice3.setText(s3.split("-")[0]);
+                choice3.setTag(s3.split("-")[1]);
+
+                Button[] buttons = {choice1, choice2, choice3};
+
+                Button correct = buttons[new Random().nextInt(buttons.length)];
+
+                getNewLyrics(correct);
             }
         });
+
     }
 
-    void getNewLyrics() {
+    void getNewLyrics(Button correct) {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    "https://api.lyrics.ovh/v1/Nas/Halftime",
+                    "https://api.lyrics.ovh/v1/" + correct.getText() + "/" + correct.getTag(),
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
