51019ac598766f92fe6257bff8869f2bbee0d299
andrewbraxton <braxton3@illinois.edu>
2018-12-02 15:14:23 -0600

    Tried to set up displaying lyrics

--- app/src/main/java/edu/illinois/cs/cs125/cs125mp6/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/cs125mp6/MainActivity.java
@@ -2,12 +2,73 @@ package edu.illinois.cs.cs125.cs125mp6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Default logging tag for messages from the main activity.
+     */
+    private static final String TAG = "MP6:Main";
+
+    /**
+     * Request queue for our network requests.
+     */
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Attach the handler to our UI button
+        final Button newLyrics = findViewById(R.id.newLyrics);
+        newLyrics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "New Lyrics button clicked");
+                getNewLyrics();
+            }
+        });
+    }
+
+    void getNewLyrics() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.lyrics.ovh/v1/Nas/Halftime",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d(TAG, response.toString());
+                            final TextView lyrics = findViewById(R.id.lyrics);
+                            lyrics.setText(response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.w(TAG, error.toString());
+                        }
+                    });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
